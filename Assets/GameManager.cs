031c84ce9273448496dc6814e61f4c952076cd9c
Sky Rolnick <skymeson@gmail.com>
2017-03-02 08:33:56 -0500

    Homography Shader
    Using quad warp homography shader

---
+++ Assets/GameManager.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour {
+
+    public Button yourButton;
+
+    void Start()
+    {
+        Button btn = yourButton.GetComponent<Button>();
+        btn.onClick.AddListener(TaskOnClick);
+    }
+
+    public void TaskOnClick()
+    {
+        Debug.Log("You have clicked the button!");
+        
+        //Instantiate("MalableQuad", Vector3(0, 0, 0), Quaternion.identity);
+    }
+}
